a8890e4f02d262f21d8d4d0fb155619a1f4f1cee
eyeland <63192606+eyeland@users.noreply.github.com>
2022-03-02 00:36:55 -0500

    first commit

---
+++ input.js
@@ -0,0 +1,17 @@
+export class InputHandler {
+    constructor(paddle) {
+        document.addEventListener('keydown', event => {
+
+            switch (event.keyCode) {
+                case 37: 
+                    paddle.moveLeft();
+                    break;
+
+                case 39: 
+                    alert('move right');
+                    break;    
+            }
+        })
+    }
+
+}
